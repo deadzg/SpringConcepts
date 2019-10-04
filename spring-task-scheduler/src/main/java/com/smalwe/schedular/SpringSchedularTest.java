e8264a1f5aab5fe88c1b31a5be0c4deefbc3115b
soumalwe <sourabh.malwe@publicis.sapient.com>
2019-10-04 09:18:03 +0100

    Adding schedular task

---
+++ spring-task-scheduler/src/main/java/com/smalwe/schedular/SpringSchedularTest.java
@@ -0,0 +1,23 @@
+package com.smalwe.schedular;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class SpringSchedularTest {
+    private static final Logger log = LoggerFactory.getLogger(SpringSchedularTest.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportTime() {
+        log.info("Reporting time : {}", dateFormat.format(new Date()));
+    }
+
+}
